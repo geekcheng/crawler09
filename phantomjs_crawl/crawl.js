8c5dafd0fc6268c5ea8dce1f246bfb44e7555ee4
devqin <newqintengfei@gmail.com>
2013-04-24 21:31:25 +0800

    cral.js

--- phantomjs_crawl/crawl.js
+++ phantomjs_crawl/crawl.js
@@ -6,20 +6,13 @@ page.open(url, function(status) {
 		page.evaluate(function() {
 		var tc =document.body.innerText;
 		var href=window.location.href;
-			//抓取内容返回到nodejs进行持久化处理。
 			$.ajax({
 				type: "POST",
 				url:"http://127.0.0.1:1337/ajax",
 				dataType:"text",
 				data:{"url":href,"data":tc}
-				//success:function(){
-				//console.log("crawler09 has been crawler "+href+" at "+Date.now());
-				//}
 			});
-			//return href+tc;
 	    });
-	    //console.log(title);
-	    //phantom.exit();
 	});
 
 });

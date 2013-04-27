f33c0bcb0be89dc573f3f4f40696a17d6f76909c
devqin <newqintengfei@gmail.com>
2013-04-28 01:23:47 +0800

    crawl.js

--- phantomjs_crawl/crawl.js
+++ phantomjs_crawl/crawl.js
@@ -1,10 +1,9 @@
 var page = require('webpage').create();
-var url="http://toozhao.com/";
-console.log("open at<-"+Date.now()+"->");
+//var url="http://toozhao.com/";
 page.open(url, function(status) {
 //console.log("open at<-"+Date.now()+"->");
 	page.includeJs("http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js",function(){
-		page.evaluate(function() {
+	var sss=page.evaluate(function() {
 		var st=Date.now();
 		var tc =document.body.innerText;
 		var href=window.location.href;
@@ -25,28 +24,26 @@ page.open(url, function(status) {
 
 		var len = as.length;
 		for(var i=0;i<len;i++){
-		var cura=as[i].getAttribute("href");
-		if(as[i].getAttribute("href").indexOf("http")=="-1"){
-		
-			ha.push(href+cura.substr(1));
-		}else{
-			ha.push(cura);
-		}
+			var cura=as[i].getAttribute("href");
+			if(as[i].getAttribute("href").indexOf("http")=="-1"){
+				ha.push(href+cura.substr(1));
+			}else{
+				ha.push(cura);
+			}
 		}
 			$.ajax({
 				type: "POST",
 				url:"http://127.0.0.1:1337/ajax",
-				dataType:"text",
+				dataType:"json",
 				data:{"url":href,"data":tc,"suba":ha},
-				success:function(){
-				}
+				async:false
 			});
-
-				    	
+						return href;
+  	
 	    });
-
+	    console.log(sss);
+	    phantom.exit();
 	});
-				phantom.exit();
 
 });
 
